ade46405ebc8ed130b05033c1320c720ed208b9d
Toprak Koç <hello@toprak.dev>
2020-10-14 19:14:39 +0300

    created  movie item component

---
+++ components/MovieItem.js
@@ -0,0 +1,18 @@
+class MovieItem extends HTMLElement {
+    connectedCallback () {
+        let  componentHtml = 
+            '<div class="image">' +
+            '<img src="' + (this.getAttribute('poster') || '') + '"/>' +
+            '</div>' +
+            '<div class="content">' +
+            '<div class="header">' + (this.getAttribute('name') || '') + '</div>' + 
+            '<div class="meta">' + 
+            '<a>' + (this.getAttribute('director') || '') + '</a>' +
+            '</div>' +
+            '<div class="description">' + (this.getAttribute('plot') || '') + '</div>';
+
+        this.innerHTML = componentHtml;
+    };
+  }
+
+  customElements.define('movie-item', MovieItem);
