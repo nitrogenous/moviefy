2502b131749a7cdbb6dbf9e6de92fbe0b05db1ce
Toprak Koç <hello@toprak.dev>
2020-10-15 14:57:49 +0300

    whitespace fix

--- components/MessageItem.js
+++ components/MessageItem.js
@@ -1,14 +1,14 @@
 class MessageItem extends HTMLElement {
     connectedCallback () {
         let componentHtml = '<i class="close icon" id="' + this.getAttribute('elementId') + '-close"></i>' +
-            '<div class="header">' + this.getAttribute('message') + '</div>';        
+            '<div class="header">' + this.getAttribute('message') + '</div>';   
+                 
         let componentStyle = 'display: flex;' + 
         'justify-content: center;';
 
         this.setAttribute('class','ui ' + this.getAttribute('type') + ' message');
         this.setAttribute('id', this.getAttribute('elementId'));
         this.setAttribute('style', componentStyle);
-
         this.innerHTML = componentHtml;
     };
 }
