529570f8c12b84b28a7009cf732799512852ca2f
Toprak Koç <hello@toprak.dev>
2020-10-15 11:08:27 +0300

    implemented design into the component

--- components/MessageItem.js
+++ components/MessageItem.js
@@ -1,10 +1,14 @@
 class MessageItem extends HTMLElement {
     connectedCallback () {
         let componentHtml = '<i class="close icon" id="message-item-close"></i>' +
-            '<div class="header">' + this.getAttribute('message') + '</div>';
-        
+            '<div class="header">' + this.getAttribute('message') + '</div>';        
+        let componentStyle = 'display: flex;' + 
+        'justify-content: center;';
+
         this.setAttribute('class','ui ' + this.getAttribute('type') + ' message');
+        this.setAttribute('style', componentStyle);
         this.setAttribute('id', 'message-item');
+        
         this.innerHTML = componentHtml;
     };
 }
