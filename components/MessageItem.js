7abcec70610cf5df644fda69325819e4047b0dd9
Toprak Koç <hello@toprak.dev>
2020-10-15 11:40:12 +0300

    changed name of attribute

--- components/MessageItem.js
+++ components/MessageItem.js
@@ -1,12 +1,12 @@
 class MessageItem extends HTMLElement {
     connectedCallback () {
-        let componentHtml = '<i class="close icon" id="' + this.getAttribute('itemId') + '-close"></i>' +
+        let componentHtml = '<i class="close icon" id="' + this.getAttribute('elementId') + '-close"></i>' +
             '<div class="header">' + this.getAttribute('message') + '</div>';        
         let componentStyle = 'display: flex;' + 
         'justify-content: center;';
 
         this.setAttribute('class','ui ' + this.getAttribute('type') + ' message');
-        this.setAttribute('id', this.getAttribute('itemId'));
+        this.setAttribute('id', this.getAttribute('elementId'));
         this.setAttribute('style', componentStyle);
 
         this.innerHTML = componentHtml;
