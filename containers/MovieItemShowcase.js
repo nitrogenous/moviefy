364b9c12047855178cd2e8fbad13348680cfbb2a
Toprak Koç <hello@toprak.dev>
2020-10-16 16:33:20 +0300

    fixed component name and removed some styles

--- containers/MovieItemShowcase.js
+++ containers/MovieItemShowcase.js
@@ -1,14 +1,12 @@
-class ItemShowcase extends HTMLElement {
+class MovieItemShowcase extends HTMLElement {
     connectedCallback () {
         var componentStyle =  'display: grid;' + 
             'grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));' +
             'grid-gap: .5rem;' + 
             'grid-auto-flow: column;' + 
             'grid-auto-columns: minmax(260px, 1fr);' + 
-            'margin-left: 7.5vw;' +
-            'width: 85vw;' + 
             'min-height: 620px;' + 
-            'overflow-y: hidden;' + 
+            'overflow-y: hidden;' +
             'overflow-x: scroll;';
         var componentHtml = '<h1 class="ui header">' + this.getAttribute('title') + "</h1>" +
             '<div class="ui link cards" id="' + this.getAttribute('showcaseId') + '" style="' + componentStyle + '" ></div>';
@@ -17,4 +15,4 @@ class ItemShowcase extends HTMLElement {
     };  
 }
 
-customElements.define('item-showcase', ItemShowcase);
+customElements.define('movie-item-showcase', MovieItemShowcase);
